268a62ac1ed5a6a963231afc3cac8531cb47bee3
dani <joyjoy312@naver.com>
2023-06-12 20:17:41 +0900

    - Student클래스 생성, 객체 활용

--- src/main/java/com/example/spring_practice1/MvcController.java
+++ src/main/java/com/example/spring_practice1/MvcController.java
@@ -7,10 +7,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class MvcController {
 
+    //RequestMapping메소드는 Model model 이라는 인자를 가지고 있습니다.
     @RequestMapping("/")
     public String home(Model model){
         model.addAttribute("message","메시지 입력 내용");
         System.out.println(model);
         return "home";
+    }
+
+    @RequestMapping("/student")
+    public String student(Model model) {
+        //html에서 Student객체 활용하도록 함
+        model.addAttribute("object", new Student("yedi", "yedi@gmail.com"));
+        return "student";
     }
 }
