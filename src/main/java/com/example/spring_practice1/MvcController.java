f8d47ce12a3f74e2ec6d35d9f9e7cf4d746d7ffc
dani <joyjoy312@naver.com>
2023-06-12 21:11:56 +0900

    - each문으로 Student객체 반복

--- src/main/java/com/example/spring_practice1/MvcController.java
+++ src/main/java/com/example/spring_practice1/MvcController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -17,12 +18,11 @@ public class MvcController {
         System.out.println(model);
         return "home";
     }
-
     @RequestMapping("/student")
     public String student(Model model) {
         //html에서 Student객체 활용하도록 함
         model.addAttribute("object", new Student("yedi", "yedi@gmail.com"));
-//        model.addAttribute("object2", new Student("bibi", "bibi@gmail.com"));
+        model.addAttribute("object2", new Student("bibi", "bibi@gmail.com"));
         return "student";
     }
 
@@ -38,5 +38,15 @@ public class MvcController {
         List<String> listOfStrings = Arrays.asList("foo", "bar", "baz");
         model.addAttribute("itemList", listOfStrings);
         return "each";
+    }
+    @RequestMapping("/students")
+    public String students(Model model) {
+        List<Student> studentList = Arrays.asList(
+                new Student("Alex","Alex@gmail.com"),
+                new Student("Brad","Brad@gmail.com"),
+                new Student("Chad","Chad@gmail.com")
+        );
+        model.addAttribute("studentList", studentList);
+        return "students";
     }
 }
